e29e7a5bbec893127a122af4789a5cc193828017
Katie Atkins <katherine.atkins@yale.edu>
2014-08-11 15:00:56 -0400

    hospitalized can now become infected by hospitalized (betaH) and workers (betaW); workers can now become infected by hospitalized (betaW) and workers (betaI)

--- NewModelCode/Tau_leap.m
+++ NewModelCode/Tau_leap.m
@@ -25,15 +25,28 @@ Nh = Sh+Eh+Ih+Fh+Rh+Dh;
 Nw = Sw+Ew+Iw+Fw+Rw+Dw;
 N = Ng+Nf+Nh+Nw;
 Change = zeros(30,24); % 30 is no of events and 24 is no of compartments
-% Transitions
+Rate = zeros(30,1);
+%% Transitions
+% General: susc -> exposed
 Rate(1) =epsilon*betaI*Sg*Ig/Ng;                          Change(1,1) = -1; Change(1,5) = +1;
+% Funeral: susc -> exposed
 Rate(2) = epsilon*betaF*Sf;                               Change(2,2) = -1; Change(2,6) = +1;
-Rate(3) = epsilon*betaH1*Sh*Ih/Nh;                        Change(3,3) = -1; Change(3,7) = +1;
-Rate(4) = epsilon*betaW*Sw*Ih/Nh;                         Change(4,4) = -1; Change(4,8) = +1;
+% Hosp: susc -> exposed
+Rate(3) = epsilon*(betaH1*Sh*Ih/Nh + betaW*Sh*Iw/Nw);     Change(3,3) = -1; Change(3,7) = +1;  %could we have transmissibility between hospitalized patients be same as in general popn?
+% Worker: susc -> exposed
+Rate(4) = epsilon*(betaW*Sw*Ih/Nh + betaI*Sw*Ih/Nh);      Change(4,4) = -1; Change(4,8) = +1;
+
+
+% General: exposed -> inf
 Rate(5) = alpha*Eg;                                      Change(5,5) = -1; Change(5,9) = +1;
+% Funeral: exposed -> inf
 Rate(6) = alpha*Ef;                                      Change(6,6) = -1; Change(6,10) = +1;
+% Hosp: exposed -> inf
 Rate(7) = alpha*Eh;                                      Change(7,7) = -1; Change(7,11) = +1;
+% Worker: exposed -> inf
 Rate(8) = alpha*Ew;                                      Change(8,8) = -1; Change(8,12) = +1;
+
+
 Rate(9) = delta1*(1-theta)*gammaD*Ig;                    Change(9,9) = -1; Change(9,13) = +1;
 Rate(10) = delta1*(1-theta)*gammaD*If;                   Change(10,10) = -1; Change(10,14) = +1;
 Rate(11) = delta2*gammaDH*Ih;                            Change(11,11) = -1; Change(11,15) = +1;
@@ -46,7 +59,7 @@ Rate(17) = gammaF*Fg;                                    Change(17,13) = -1; Cha
 Rate(18) = gammaF*Ff;                                    Change(18,14) = -1; Change(18,22) = +1;
 Rate(19) = gammaF*Fh;                                    Change(19,15) = -1; Change(19,23) = +1;
 Rate(20) = gammaF*Fw;                                    Change(20,16) = -1; Change(20,24) = +1;
-Rate(21) = fGF*F/N;                                     Change(21,1) = -1; Change(21,2) = +1;
+Rate(21) = fGF*F/N;                                      Change(21,1) = -1; Change(21,2) = +1;
 Rate(22) = fFG*Sf;                                       Change(22,2) = -1; Change(22,1) = +1;
 Rate(23) = fGH*Sg;                                       Change(23,1) = -1; Change(23,3) = +1;      
 Rate(24) = fHG*Sh;                                       Change(24,3) = -1; Change(24,1) = +1;
@@ -60,8 +73,11 @@ Rate(30) = (1-epsilon)*betaW*Sw*(Ih/Nh+Iw/Nw);            Change(30,4) = -1; Cha
 
 new_value=old;
 
-for i=1:30
+for i=1:size(Rate,1)
     Num=poissrnd(Rate(i)*tau);
+%     if i==4
+%         
+%     end
     %% Make sure things don't go negative
     Use=min([Num new_value(find(Change(i,:)<0))]);
     new_value=new_value+Change(i,:)*Use;

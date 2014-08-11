bfee62d74bfb83446df90a93af4c4230b94c923c
Katie Atkins <katherine.atkins@yale.edu>
2014-08-11 16:58:57 -0400

    change epsilon to be proportion of Symptomatic individuals consistent with equations

--- NewModelCode/EbolaModel.m
+++ NewModelCode/EbolaModel.m
@@ -6,7 +6,7 @@ function [] = EbolaModel()
     betaW = 0.794/7;    % Transmission coefficient for hospital/ebola treatment workers
     betaF = 0.794/7;    %/7;% 7.653/7;   % Transmission coefficient during funerals with ebola patient
     alpha = 1/7;        % 1/alpha: mean duration of the incubation period  
-    theta = 0.67;       % Percentage/proportion of infectious cases are hospitaized
+    theta = 67/100;       % Percentage of infectious cases are hospitaized
     gammaH = 1/5;       % 1/gammaH: mean duration from symptom onset to hospitalization
     gammaI = 1/10;      % 1/gammaI: mean duration of the infectious period for survivors
     gammaD = 1/9.6;     % 1/gammaD: mean duration from onset to death
@@ -19,23 +19,23 @@ function [] = EbolaModel()
     fFG = 1/2;          % 1/average time spent at close quarters with body at funeral
     fGH = 90272 / (67.8e6 * 365);  % rate of hospitalization per person per day (DRC 2012 estimates)
     fHG = 1/7;          % 1/average time spent at in hospital with non-ebola disease
-    epsilon = 0.1;       % asymptotic illness proportion
+    epsilon = 10/100;       % percentage Symptotic illness 
 
     N0 = 4.4e6;         % Initial population size
     
     % Initial conditions
     Eg0 = 0; Ef0 = 0; Eh0 = 0; Ew0 = 0; 
-    Ig0 = 0; If0 = 0; Ih0 = 0; Iw0 = 0;
+    Ig0 = 1; If0 = 0; Ih0 = 0; Iw0 = 0;
     Fg0 = 0; Ff0 = 0; Fh0 = 0; Fw0 = 0;
     Rg0 = 0; Rf0 = 0; Rh0 = 0; Rw0 = 0;
     Dg0 = 0; Df0 = 0; Dh0 = 0; Dw0 = 0;
     
-    Sh0 = 0; Sf0 = 0; Sw0 = (0.1/10000)*N0;  Sg0 = N0 - Sh0 - Sw0 - Ig0; 
+    Sh0 = 3*(0.1/10000)*N0; Sf0 = 0; Sw0 = (0.1/10000)*N0;  Sg0 = N0 - Sh0 - Sw0 - Ig0; 
     
     % Tau and maximum time taken
     tau=1;
     MaxTime=1*365;
-    MaxIt = 20;
+    MaxIt = 10;
 
     initial = [Sg0,Sf0,Sh0,Sw0,Eg0,Ef0,Eh0,Ew0,Ig0,If0,Ih0,Iw0,Fg0,Ff0,Fh0, Fw0,Rg0,Rf0,Rh0,Rw0,Dg0,Df0,Dh0,Dw0];
 

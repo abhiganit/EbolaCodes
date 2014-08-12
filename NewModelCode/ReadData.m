48de046ef9d65c52866a08686b9bc493ca442e58
Katie Atkins <katherine.atkins@yale.edu>
2014-08-12 18:56:50 -0400

    strip out NaNs from data set -- this is done separately for each set of data (e.g. different times for deaths counts and case counts

--- NewModelCode/ReadData.m
+++ NewModelCode/ReadData.m
@@ -10,11 +10,24 @@ function data = ReadData
     dataout = textscan(fid, dataformat, 'Delimiter', '\t');
     
     fclose(fid);
-    
+   
     % save as separate cells
     for i = 1:size(headerlines,2)
         header = headerlines{i};
         header = header{1};
         data.(header) = dataout{i};
+    end
+    
+    allfields = fieldnames(data);
+    datafields = allfields(2:2:end,1);
+    timefields = allfields(1:2:(end-1),1);
+    
+    % strip out all the NaNs separately for each dataset
+   
+    for i = 1:size(datafields,1)
+        nanfield = datafields{i};
+        timefield = timefields{i};
+        data.(nanfield) = data.(nanfield)(~isnan(data.(nanfield)));
+        data.(timefield) = data.(timefield)(~isnan(data.(nanfield)));
     end
 end

cc18a6e51cbbba3ede5918edecab3d9cb25b1fd6
Katie Atkins <katherine.atkins@yale.edu>
2014-08-12 18:13:23 -0400

    fix field names and sum up difference between data and model

--- NewModelCode/ErrorFunction.m
+++ NewModelCode/ErrorFunction.m
@@ -1,14 +1,21 @@
 
 
-function f = ErrorFunction(data, model)
-
+function f = ErrorFunction(beta)
+    
+    % get model and data
+    data = ReadData();
+    model = EbolaModel(1, beta);
+    
+    % clean the data and scroll through actual incidences at given times
     allfields = fieldnames(data);
     fields = allfields(2:2:end,1);
     index = 0;
     for field = fields
+       field = field{1};
        index = index + 1;
-       parf(index) = (data.(field) - model.(field)).^2;
+       partsumf(index) = sum((data.(field) - model.(field)).^2);
     end
     
-    f = sum(parf);
+    % return sum of square differences
+    f = sum(partsumf);
 end

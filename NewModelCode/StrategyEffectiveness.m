5ca5909e4961c6e04ed89b34d4715ad07003428c
Katie Atkins <katherine.atkins@yale.edu>
2014-08-23 22:11:15 -0400

    make ci plots

--- NewModelCode/StrategyEffectiveness.m
+++ NewModelCode/StrategyEffectiveness.m
@@ -3,12 +3,25 @@ function[B] = StrategyEffectiveness(input)
 % m x n.
 fig = figure;
 set(fig, 'Position', [500, 100, 900, 500])
-A = input;
-s = length(A); % No of strategies
-m = length(A{1}(:,1)); % No of days after strategies
-n = length(A{1}(1,:)); % Variation in rates for strategies
+Adet = input;
+load('ExtinctionProbs');  %load stochastic simulation data
+Asto = A;
+ExtProbs = B;
+outputtimes = [30, 91, 183];
 
-B = cell(1,s);
+for i=1:size(Asto,2)
+    cilow{i} = cellfun( @arraySortLow ,Asto{i}, repmat({outputtimes},1,4), repmat({size(Asto{1}{1},1)},1,4), 'UniformOutput', false );
+    cihigh{i} = cellfun( @arraySortHigh ,Asto{i}, repmat({outputtimes},1,4), repmat({size(Asto{1}{1},1)},1,4), 'UniformOutput', false );
+    cilowmat{i} = cell2mat(cilow{i}')';
+    cihighmat{i} = cell2mat(cihigh{i}')';
+end
+
+
+s = length(Adet); % No of strategies
+m = length(Adet{1}(:,1)); % No of days after strategies
+n = length(Adet{1}(1,:)); % Variation in rates for strategies
+
+Bdet = cell(1,s);
 t = 1:m-1;
 
 
@@ -45,7 +58,7 @@ legendtitle = {'Community Transmission Reduction','','', ...
 strategies = 4:9;
 for i = 1:s
     for j = 1:n
-        B{i}(:,j) = diff(A{i}(:,j));
+        Bdet{i}(:,j) = diff(Adet{i}(:,j));
     end
 end
         
@@ -65,7 +78,7 @@ for i = strategies
     subplot(6,3,subplotorder(i-3))    
     for j=2:n
         hold on;
-        plot(t,B{i}(:,j), 'Color', cmap1(j-1,:), 'LineWidth', 1.2);
+        plot(t,Bdet{i}(:,j), 'Color', cmap1(j-1,:), 'LineWidth', 1.2);
     end
     xlabel('Months After Intervention', 'FontSize', 14)
     set(gca, 'FontSize',labelsize)
@@ -84,33 +97,43 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%% plotting CUMULATIVE CASES  %%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-outputtimes = [30, 91, 183];
 
 xlabstr = {'1 mo', '3 mo', '6 mo'};
-Bsums = cellfun(@(a)cumsum(a), B, 'UniformOutput',false);
-Boutputsums = cellfun(@(a,times)a(times,:), Bsums, repmat({outputtimes},1,9), 'UniformOutput',false);
+Bsumsdet = cellfun(@(a)cumsum(a), Bdet, 'UniformOutput',false);
+Boutputsumsdet = cellfun(@(a,times)a(times,:), Bsumsdet, repmat({outputtimes},1,9), 'UniformOutput',false);
 
+
+%differences
+upperdifference = cellfun( @(u,m) u-m(:,2:end), cihighmat, Boutputsumsdet(4:9), 'UniformOutput', false);
+lowerdifference = cellfun( @(l,m) m(:,2:end)-l, cilowmat, Boutputsumsdet(4:9), 'UniformOutput', false);
 subplotorder = [4,5,6,13,14,15];
 
 colormap(cmap1)
 for i = strategies
-    subplot(6,3,subplotorder(i-3))
-    bar(Boutputsums{i}(:,2:end))
+    index = strategies-3;
+    subplot(6,3,subplotorder(index))
+    hold on;
+    bar(Boutputsumsdet{i}(:,2:end))
+    plot([1,2,3], Boutputsumsdet{i}(:,2:end) + upperdifference{i});
+    plot([1,2,3], Boutputsumsdet{i}(:,2:end)-lowerdifference{i});
     ylim([0 3.4e3])
     set(gca,'XTickLabel',{'','',''}, 'FontSize', labelsize);
     set(gca, 'YTick', [0, 1000, 2000,3000])
     box off;
 end
 
+
+
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%% plotting PROBS OF EXTINCTION  %%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 colormap(cmap2);
 subplotorder = [7,8,9,16,17,18];
-load('ExtinctionProbs');
+
 for i = 1:6
     subplot(6,3,subplotorder(i))
-    bar(B{i}');
+    bar(ExtProbs{i}');
     set(gca,'XTickLabel',xlabstr, 'FontSize', labelsize)
     ylim([0,1.05])
     box off;
@@ -209,6 +232,17 @@ annotation('textbox', [0 0.05 0.05 0.1],...,...
 end
    
 
+function ciestlow = arraySortLow(arr, inputcolumns, N)
+    loCI = max(floor(N*0.025),1);
+    sortedcolumns = sort(arr(:,inputcolumns));
+    ciestlow  = sortedcolumns(loCI,:);
+end
 
+function ciesthigh = arraySortHigh(arr, inputcolumns, N)
+
+    hiCI = max(ceil(N*0.975),1);
+    sortedcolumns = sort(arr(:,inputcolumns));
+    ciesthigh  = sortedcolumns(hiCI,:);
+end
             
  

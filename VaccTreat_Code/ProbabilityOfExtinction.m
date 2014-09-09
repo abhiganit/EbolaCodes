1cdea53230fc9c3f5d3a6f0a617280f8d5ac1885
Katie Atkins <katherine.atkins@yale.edu>
2014-09-09 12:46:59 -0400

    change thresholds

--- VaccTreat_Code/ProbabilityOfExtinction.m
+++ VaccTreat_Code/ProbabilityOfExtinction.m
@@ -1,4 +1,4 @@
-function[ProbCell,ThresholdEffandCov3months,ThesholdEffandCov6months] = ProbabilityOfExtinction(input, sliceprob)
+function[ProbCell,ThresholdEffandCov3months,ThresholdEffandCov6months] = ProbabilityOfExtinction(input, sliceprob)
 %INPUT:
 % input is a cell of size 1 x p where p is number of delays
 % each element of input is a cell of side q x r where q is the variation
@@ -13,7 +13,7 @@ function[ProbCell,ThresholdEffandCov3months,ThesholdEffandCov6months] = Probabil
 % each element of the q x r cells is a matrix of size 1 x 2 whose elements
 % are probabilities of < 1 daily cases after 3 and 6 months repectively.
 A = input;
-p = length(A); % Number of delays
+p = length(A)-1 ; % Number of delays
 q = length(A{1}(:,1)); % Number of variations in efficacy between 0 and 1
 r = length(A{1}(1,:)); %  Number of variations of coverage between 0 and 1
 n = length(A{1}{1,1}(:,1)); % Number of stochastic runs
@@ -26,7 +26,7 @@ for i = 1:p            % Looping over delays
             C = A{i}{j,k}';   % Grabbing and transposing the ith delay, jth efficacy and kth coverage matrix
             B = diff(C);      % Calculating daily new cases
             month3 = sum(B(91,:)<1); % # stochastic realizations resulting into < 1 daily ebola cases after 3 months
-            month6 = sum(B(183,:)<1);  % # stochastic realizations resulting into < 1 daily ebola cases after 3 months
+            month6 = sum(B(183,:)<1);  % # stochastic realizations resulting into < 1 daily ebola cases after 6 months
             ProbIntermediateCell{j,k} = [month3,month6]./n; 
         end
     end
@@ -42,8 +42,17 @@ for i = 1:p
         Y = X>=sliceprob;
         ind3 = find(Y(:,1),1,'first');
         ind6 = find(Y(:,2),1,'first');
-        EC3(j,:) = [eff(j),cov(ind3)];
-        EC6(j,:) = [eff(j),cov(ind6)];
+        if size(ind3,1)==0
+            EC3(j,:) = [eff(j),NaN];
+        else
+            EC3(j,:) = [eff(j),cov(ind3)];
+        end
+        if size(ind6,1)==0
+            EC6(j,:) = [eff(j),NaN];
+        else
+            EC6(j,:) = [eff(j),cov(ind6)];
+        end
+        
     end
     ThresholdEffandCov3months{i} = EC3;
     ThresholdEffandCov6months{i} = EC6;
